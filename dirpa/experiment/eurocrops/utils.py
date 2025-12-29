25f2523cf2f5a407551d712d65d9245d8bb16927
joana.reuss <joana.reuss@tum.de>
2025-12-29 14:41:58 +0000

    load only label files to access class id names

--- dirpa/experiment/eurocrops/utils.py
+++ dirpa/experiment/eurocrops/utils.py
@@ -1,18 +1,32 @@
 import logging
+from functools import cache
 from pathlib import Path
 from typing import Literal, cast
 
+import pandas as pd
 from eurocropsml.dataset.config import (
     EuroCropsDatasetConfig,
     EuroCropsDatasetPreprocessConfig,
     EuroCropsSplit,
 )
-from eurocropsml.dataset.preprocess import get_class_ids_to_names
+
+# from eurocropsml.dataset.preprocess import get_class_ids_to_names
+from eurocropsml.dataset.preprocess import read_metadata
 
 from dirpa.dataset.eurocrops.train import load_dataset_split
 from dirpa.dataset.task import Task
 
 logger = logging.getLogger(__name__)
+
+
+# TODO: remove once this is incorporated into eurocropsml
+@cache
+def get_class_ids_to_names(raw_data_dir: Path) -> dict[str, str]:
+    """Get a dictionary mapping between class identifiers and readable names."""
+    labels_df: pd.DataFrame = read_metadata(raw_data_dir.joinpath("labels"))
+    unique_labels_df = labels_df.drop_duplicates()
+    ids_to_names_dict = unique_labels_df.set_index("EC_hcat_c").to_dict()["EC_hcat_n"]
+    return {str(k): v for k, v in ids_to_names_dict.items()}
 
 
 def load_task(
@@ -22,6 +36,7 @@ def load_task(
     preprocess_config: EuroCropsDatasetPreprocessConfig,
     dataset_config: EuroCropsDatasetConfig,
     max_samples: int | str = "all",
+    downsample_classes: list[tuple[int, float]] | None = None,
 ) -> Task:
     """Load pretraining or finetuning task from the EuroCrops dataset."""
 
@@ -42,5 +57,6 @@ def load_task(
             dataset_config=dataset_config,
             max_samples=max_samples,
             class_ids_to_names=get_class_ids_to_names(preprocess_config.raw_data_dir),
+            downsample_classes=dict(downsample_classes) if downsample_classes is not None else None,
         ),
     )
